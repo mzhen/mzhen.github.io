e726e8c5e97617210e84f252f8a351a370d44df4
mzhen <mzhen@risd.edu>
2015-05-04 01:34:47 -0400

    kirby

---
+++ kirby-2.0.2/site/accounts/mzhen.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: mzhen
+email: mi4zhe@gmail.com
+password: >
+  $2a$10$D7ZZQoILnQm.Qy5RroXfMOSe0k7Pg685r2lo.ssqzEmrHcMQCsqUy
+language: en
+role: admin
+token: 9d082d8e0b679c0f9273ce7fe7cbcd3f9344d9c6
